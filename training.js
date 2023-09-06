ec72d311b740a1b35daacfd6367331b0e6c460b1
LindaSz0112 <lindaszarvas@gmail.com>
2023-09-06 18:12:12 +0200

    resolved sumMix challenge

--- training.js
+++ training.js
@@ -832,3 +832,14 @@ function greetNew(language) {
 console.log(greetNew("english"));
 console.log(greetNew("dutch"));
 console.log(greetNew("IP_ADDRESS_INVALID"));
+
+function sumMix(arr) {
+  const arrOfNumbers = arr
+    .map((el) => Number(el))
+    .reduce((acc, currentValue) => acc + currentValue, 0);
+  return arrOfNumbers;
+}
+
+console.log(sumMix([9, 3, "7", "3"]));
+console.log(sumMix(["5", "0", 9, 3, 2, 1, "9", 6, 7]));
+console.log(sumMix(["3", 6, 6, 0, "5", 8, 5, "6", 2, "0"]));
