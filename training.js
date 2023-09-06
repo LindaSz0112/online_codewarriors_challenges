c1fc5e4e286e0157e26b492bb1d9b3a35f3007a3
LindaSz0112 <lindaszarvas@gmail.com>
2023-09-06 18:18:00 +0200

    Starting count challenge

--- training.js
+++ training.js
@@ -843,3 +843,13 @@ function sumMix(arr) {
 console.log(sumMix([9, 3, "7", "3"]));
 console.log(sumMix(["5", "0", 9, 3, 2, 1, "9", 6, 7]));
 console.log(sumMix(["3", 6, 6, 0, "5", 8, 5, "6", 2, "0"]));
+
+function count(string) {
+  return {};
+}
+
+console.log(count(""));
+console.log(count("a"));
+console.log(count("ab"));
+console.log(count("aba"));
+console.log(count("ABC"));
