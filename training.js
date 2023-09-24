1c12ca4208610c888ac480c3340b4e93d95d378b
LindaSz0112 <lindaszarvas@gmail.com>
2023-09-24 17:59:15 +0200

    Resolved findNeedle challenge

--- training.js
+++ training.js
@@ -1016,3 +1016,35 @@ function printerError(s) {
 console.log(
   printerError("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz")
 );
+
+function findNeedle(haystack) {
+  return haystack.includes("needle")
+    ? `found the needle at position ${haystack.indexOf("needle")}`
+    : "Your function didn't return anything";
+}
+
+console.log(
+  findNeedle([
+    "3",
+    "123124234",
+    undefined,
+    "needle",
+    "world",
+    "hay",
+    2,
+    "3",
+    true,
+    false,
+  ])
+);
+console.log(
+  findNeedle([
+    "283497238987234",
+    "a dog",
+    "a cat",
+    "some random junk",
+    "a piece of hay",
+    "needle",
+    "something somebody lost a while ago",
+  ])
+);
