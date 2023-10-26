e25b68c8d75b42e5a4415c47971f731089fd0de2
LindaSz0112 <lindaszarvas@gmail.com>
2023-10-26 19:19:12 +0200

    Resolved century challenge

--- training.js
+++ training.js
@@ -1929,3 +1929,9 @@ const otherAngle = (a, b) => 180 - (a + b);
 console.log(otherAngle(30, 60));
 console.log(otherAngle(60, 60));
 console.log(otherAngle(43, 60));
+
+const century = (year) =>
+  year % 100 === 0 ? Math.floor(year / 100) : Math.floor(year / 100) + 1;
+
+console.log(century(1705));
+console.log(century(2000));
